01a6b347b4a3f719cb5f3ea5afeba80cbd928d70
David Radley <david_radley@uk.ibm.com>
2021-02-05 08:49:08 +0000

    #60 restore correct user after refresh
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- cra-server/db/users.js
+++ cra-server/db/users.js
@@ -39,17 +39,18 @@ var records = [
 
 ];
 
-exports.findById = function(id, cb) {
-process.nextTick(function() {
-console.log("findByUserId");
-var idx = id - 1;
-if (records[idx]) {
-  cb(null, Object.assign({}, records[idx]));
-} else {
-  cb(new Error('User ' + id + ' does not exist'));
-}
-});
-}
+exports.findById = function (id, cb) {
+  process.nextTick(function () {
+    console.log("findByUserId");
+    for (var i = 0, len = records.length; i < len; i++) {
+      var record = records[i];
+      if (record.id === id) {
+        return cb(null, Object.assign({}, record));
+      }
+    }
+    return cb(new Error("User " + id + " does not exist"));
+  });
+};
 
 exports.findByUsername = function(username, cb) {
 process.nextTick(function() {
