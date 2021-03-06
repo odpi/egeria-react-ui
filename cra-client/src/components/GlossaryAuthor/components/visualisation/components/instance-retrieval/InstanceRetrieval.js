a46d48173314254d8a878be74d893a89516b53b0
David Radley <david_radley@uk.ibm.com>
2021-03-06 13:23:04 +0000

    #85 putting 1 node on the canvas with errors
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- cra-client/src/components/GlossaryAuthor/components/visualisation/components/instance-retrieval/InstanceRetrieval.js
+++ cra-client/src/components/GlossaryAuthor/components/visualisation/components/instance-retrieval/InstanceRetrieval.js
@@ -1,47 +1,51 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* Copyright Contributors to the ODPi Egeria project. */
 
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 
-import PropTypes                       from "prop-types";
+import PropTypes from "prop-types";
 import { IdentificationContext } from "../../../../../../contexts/IdentificationContext";
-import { InstancesContext }            from "../../contexts/InstancesContext";
+import { InstancesContext } from "../../contexts/InstancesContext";
 import { withRouter } from "react-router-dom";
-import "./instance-retriever.scss"
+import "./instance-retriever.scss";
 import getNodeType from "../../../properties/NodeTypes";
-const InstanceRetrieval = props => {
-
-  const instancesContext                      = useContext(InstancesContext);
-  const identificationContext = useContext(IdentificationContext);
-
-    /*
-   * Function to get node by GUID 
-   */
-  const loadNodeByGUID = () => {    
-      const urlSegments = props.match.url.split("/")
-      const guidToLoad = urlSegments[urlSegments.length - 1];
-      const secondLastSegment =   urlSegments[urlSegments.length - 2];
-      const nodeTypeKey = secondLastSegment.substring("visualise-".length);
-      console.log("nodeTypeKey" + nodeTypeKey);
-    
-      const nodeType = getNodeType(identificationContext.getRestURL("glossary-author"), nodeTypeKey);
-      console.log("nodeType" + nodeType);
-      instancesContext.loadNode(guidToLoad, nodeType);
-
-  };
-
-  return (
-    
-    <div className={props.className}>
-        {loadNodeByGUID()}
-       
-    </div>     
-  
+const InstanceRetrieval = (props) => {
+  useEffect(
+    () => {
+      loadNodeByGUID();
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
   );
 
-}
+  const instancesContext = useContext(InstancesContext);
+  const identificationContext = useContext(IdentificationContext);
 
-InstanceRetrieval.propTypes = { 
-  className  : PropTypes.string
-}
+  /*
+   * Function to get node by GUID
+   */
+  const loadNodeByGUID = () => {
+    if (props.match.url) {
+      const urlSegments = props.match.url.split("/");
+      const guidToLoad = urlSegments[urlSegments.length - 1];
+      const secondLastSegment = urlSegments[urlSegments.length - 2];
+      const nodeTypeKey = secondLastSegment.substring("visualise-".length);
+      console.log("nodeTypeKey" + nodeTypeKey);
+
+      const nodeType = getNodeType(
+        identificationContext.getRestURL("glossary-author"),
+        nodeTypeKey
+      );
+      console.log("nodeType" + nodeType);
+      //TODO pass through the onSuccess and onErrors
+      instancesContext.loadNode(guidToLoad, nodeType);
+    }
+  };
+
+  return <div className={props.className}></div>;
+};
+
+InstanceRetrieval.propTypes = {
+  className: PropTypes.string,
+};
 export default withRouter(InstanceRetrieval);
