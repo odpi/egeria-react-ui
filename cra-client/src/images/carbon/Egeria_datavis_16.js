db06699bc042c1e1a6776144fab0a91f228f1634
David Radley <david_radley@uk.ibm.com>
2021-03-06 13:52:02 +0000

    #85 remove duplicate graph button
    Signed-off-by: David Radley <david_radley@uk.ibm.com>

--- cra-client/src/images/carbon/Egeria_datavis_16.js
+++ cra-client/src/images/carbon/Egeria_datavis_16.js
@@ -3,7 +3,7 @@
 import React from "react";
 import icon from '../../imagesHolder/carbon/data-vis--1.svg';
 
-export default function Egeria_datavis_32(props) {
+export default function Egeria_datavis_16(props) {
   return (
     <img src={icon} height="16" width="16" onClick={props.onClick} alt="Data visualisation"/>
   );
