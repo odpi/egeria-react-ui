a38005346e15190783eb22b7961b28054d0d9d66
David Radley <dgradley@gmail.com>
2022-01-20 11:25:37 +0000

    git236 fix reference
    Signed-off-by: David Radley <dgradley@gmail.com>

--- cra-client/src/components/ServerAuthor/components/ServerAuthorWizard.jsx
+++ cra-client/src/components/ServerAuthor/components/ServerAuthorWizard.jsx
@@ -61,7 +61,7 @@ export default function ServerAuthorWizard() {
     setLoadingText,
     newServerConfig,
     setNewServerConfig,
-
+    setCurrentAccessServices,
     activePlatforms,
     newPlatformName,
     // available services for this servers platforms.
