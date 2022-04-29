a60e2de4e396c5e75408b337ea375e674ad518b2
David Radley <dgradley@gmail.com>
2022-04-29 12:52:11 +0100

    git427 render date time conditionally
    Signed-off-by: David Radley <dgradley@gmail.com>

--- cra-client/src/components/RepositoryExplorer/components/instance-retrieval/HistoricalDateTime.jsx
+++ cra-client/src/components/RepositoryExplorer/components/instance-retrieval/HistoricalDateTime.jsx
@@ -18,35 +18,26 @@ export default function HistoricalDateTime() {
     setErrorMessage(msg);
   };
 
-
   return (
     <div>
-    <div className="descriptive-text">
-      Historical Query
-    </div>
-    <div className="history-outline">
-      <Toggle
-        // labelA="Present"
-        // labelB="Issue Historical query"
-        id="asOfTimeToggle"
-        onToggle={instancesContext.onHistoricalQueryChange}
-      />
-      <div id="historical_date_time_picker" style={{ display: "none" }} >
-        <DateTimePicker
-          dateLabel="As of Date"
-          timeLabel="As of Time"
-          asOfDateTimeStr={instancesContext.asOfDateTimeStr}
-          onDateChange={instancesContext.onAsOfDateChange}
-          onTimeChange={instancesContext.onAsOfTimeChange}
-          dateValue={instancesContext.asOfDate}
-          timeValue={instancesContext.asOfTimeStr}
-          invalidTime={instancesContext.invalidTime}
-          invalidDate={instancesContext.invalidDate}
-          isTimeDisabled={instancesContext.isTimeDisabled}
-          onDateTimeMessage={onErrorMessage}
+      <div className="descriptive-text">Historical Query</div>
+      <div className="history-outline">
+        <Toggle
+          // labelA="Present"
+          // labelB="Issue Historical query"
+          id="asOfTimeToggle"
+          onToggle={instancesContext.onHistoricalQueryChange}
         />
-        {errorMessage}
-      </div>
+        {instancesContext.useHistoricalQuery === true && (
+          <div>
+            <DateTimePicker
+              dateLabel="As of Date"
+              timeLabel="As of Time"
+              onDateTimeMessage={onErrorMessage}
+            />
+            {errorMessage}
+          </div>
+        ) }
       </div>
     </div>
   );
